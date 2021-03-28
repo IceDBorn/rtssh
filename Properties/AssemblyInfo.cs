5b99c38fb18db97ea8ea0357e7580b75ed21a2fe
IceDBorn <fundwork@tutanota.com>
2021-03-29 00:14:29 +0300

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rtssh")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rtssh")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DD4AFF46-18C2-4E4A-9083-214DF4A6C86E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SSHStream.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using Renci.SshNet;
+
+namespace rtssh
+{
+    internal static class SSHStream
+    {
+        private static void Main(string[] args)
+        {
+            // Run RTSS or hook to existing process
+            RTSSHandler.RunRTSS();
+            
+            // Connect to SSH Client
+            var key = new PrivateKeyFile("id_rsa");
+            const string username = "icedborn";
+            const string host = "192.168.122.1";
+            const int port = 22;
+            var sshClient = new SshClient(host, port, username, key);
+            sshClient.Connect();
+            Console.WriteLine("Connected to SSH Client as " + username);
+            
+            // Stream sensors -j
+            var shellStream = sshClient.CreateShellStream("rtssh", 
+                0, 0, 0, 0, 1024);
+            const string command = "while true; do sensors -j | jq -cM; sleep 1; done";
+            shellStream.WriteLine(command);
+
+            while (true)
+            {
+                // Save ssh output into string
+                var temp = shellStream.Read();
+                
+                // Export total cpu temp from ssh output
+                try
+                {
+                    if (temp.Length > 0 && temp.StartsWith("{"))
+                    {
+                        var jsonTemp = JObject.Parse(temp);
+                        var formattedTemp = "CPU:   " +
+                                                (int) (double) jsonTemp
+                                                ["k10temp-pci-00c3"] ?
+                                                ["Tctl"] ?
+                                                ["temp1_input"] +
+                                                "";
+                        
+                        // Print cpu temp into RTSS
+                        RTSSHandler.Print(formattedTemp);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                // Wait for 1 second before updating the cpu temp
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
